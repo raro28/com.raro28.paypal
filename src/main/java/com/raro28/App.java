874413a3a22163e33711a74ebcdd23a4a8b8fb80
Hector Diaz <3kthor3adward@gmail.com>
2020-07-29 20:15:05 -0500

    initial commit

---
+++ src/main/java/com/raro28/App.java
@@ -0,0 +1,13 @@
+package com.raro28;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
