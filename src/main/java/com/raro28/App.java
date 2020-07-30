61abc62dd590cff3640dd810be515a4226a091d3
Hector Diaz <3kthor3adward@gmail.com>
2020-07-29 20:58:31 -0500

    paypal client

--- src/main/java/com/raro28/App.java
+++ src/main/java/com/raro28/App.java
@@ -1,13 +1,27 @@
 package com.raro28;
 
-/**
- * Hello world!
- *
- */
+import com.paypal.core.PayPalHttpClient;
+import com.raro28.paypal.PaypalClient;
+
 public class App 
 {
+
+    private final PaypalClient payPalClient;
+
+    private App() {
+        this.payPalClient = new PaypalClient();
+    }
+
+    private void run(){
+        PayPalHttpClient client = this.payPalClient.getClient();
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+
+        app.run();
+
+        System.out.println("end");
     }
 }
---
+++ src/main/java/com/raro28/paypal/PaypalClient.java
@@ -0,0 +1,23 @@
+package com.raro28.paypal;
+
+import com.paypal.core.PayPalEnvironment;
+import com.paypal.core.PayPalHttpClient;
+
+public class PaypalClient {
+
+    private final PayPalEnvironment environment;
+
+    private final PayPalHttpClient client;
+
+    public PaypalClient() {
+        String clientId = System.getenv("PAYPAL_CLIENT_ID");
+        String clientSecret = System.getenv("PAYPAL_CLIENT_SECRET");
+        this.environment = new PayPalEnvironment.Sandbox(clientId, clientSecret);
+
+        this.client = new PayPalHttpClient(this.environment);
+    }
+
+    public PayPalHttpClient getClient(){
+        return this.client;
+    }
+}
